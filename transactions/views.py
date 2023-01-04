557a205280bb0201290f3d961cd160e44d11d47b
gabihcaldeira <gabihcaldeira@hotmail.com>
2023-01-04 16:48:42 -0300

    feat: adicionando filtro transaction type

--- transactions/views.py
+++ transactions/views.py
@@ -7,6 +7,15 @@ class ListCreateTransactions(ListCreateAPIView):
     queryset = Transaction.objects.all()
     serializer_class = TransactionSerializer
 
+    def get_queryset(self):
+        type_parameter = self.request.GET.get('type')
+        if type_parameter:
+            queryset = Transaction.objects.filter(
+                transaction_type=type_parameter)
+            return queryset
+
+        return super().get_queryset()
+
 
 class RetrieveUpdateDestroyTransaction(RetrieveUpdateDestroyAPIView):
     queryset = Transaction.objects.all()
