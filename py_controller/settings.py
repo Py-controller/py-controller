824429c3a2a0193468d82491d0796744c2fed96b
Victor Cruz <103138539+VictrCruz312@users.noreply.github.com>
2023-01-05 11:28:39 -0300

    fix: sethings.py
    configurando o agendamento para remoção de usuários inativos por 180 dias.

--- py_controller/settings.py
+++ py_controller/settings.py
@@ -157,3 +157,6 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "users.User"
+
+
+CRONJOBS = [("0 0 * * *", "users.tasks.delete_inactive_users")]
