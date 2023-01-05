d9908836811b4ddfabc82b13850c57d7c45ddad7
unknown <brenos93@gmail.com>
2023-01-05 09:52:56 -0300

    feat: corrigindo a url de plannings

--- plannings/views.py
+++ plannings/views.py
@@ -3,7 +3,6 @@ from .serializers import PlanningSerializer
 from .models import Planning
 from django.shortcuts import get_object_or_404
 from categories.models import Categories
-import ipdb
 
 
 class PlanningView(generics.ListCreateAPIView):
@@ -15,14 +14,6 @@ class PlanningView(generics.ListCreateAPIView):
         category, _ = Categories.objects.get_or_create(name=pop_categories)
         request.data["category"] = category.id
         return super().create(request, *args, **kwargs)
-
-    # def perform_create(self, serializer):
-    #     ipdb.set_trace()
-    #     # category = get_object_or_404(
-    #     #     Categories,
-    #     #     name=self.kwargs[self.request.data["category"]],
-    #     # )
-    #     serializer.save(category=self.request.data["category"])
 
 
 class PlanningDetailView(generics.RetrieveUpdateDestroyAPIView):
