d670f58ba5c6f4297066c8cc7d0d69fa6e93c36f
alucard93 <m.vinicius93.brasilmortal@gmail.com>
2023-01-04 22:15:15 -0300

    feat: Urls.py
    Criação da urls.py de accounts

---
+++ accounts/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("accounts/", views.AccountView.as_view()),
+    path("accounts/<str:account_uuid>/", views.AccountDetailView.as_view()),
+    path("accounts/<str:account_uuid>/transactions", views.AccountTransactionView.as_view()),
+    # path("accounts/<str:account_uuid>/plannings", views.AccountPlanningView.as_view()),
+]
