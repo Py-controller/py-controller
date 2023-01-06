5e1adc5de650223179f683d182ba583f8b6fe8db
VictrCruz312 <motoggb979@gmail.com>
2023-01-06 15:48:00 -0300

    fix: adicionando localhost aos allowed_hosts e corrigindo o volume do docker que estava o caminho errado

--- py_controller/settings.py
+++ py_controller/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv("SECRET_KEY", get_random_secret_key())
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["0.0.0.0", "127.0.0.1"]
+ALLOWED_HOSTS = ["0.0.0.0", "127.0.0.1", "localhost"]
 
 RENDER_EXTERNAL_HOSTNAME = os.getenv("RENDER_EXTERNAL_HOSTNAME")
 if RENDER_EXTERNAL_HOSTNAME:
