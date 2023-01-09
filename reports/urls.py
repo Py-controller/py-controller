2ac6b6fb1b9fe485719ed0d682714a8b61840de0
gabihcaldeira <gabihcaldeira@hotmail.com>
2023-01-09 18:19:41 -0300

    feat: adicionando rota e view de reports

--- reports/urls.py
+++ reports/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('reports/', views..as_view()),
-    path('reports/plannings/', views..as_view())
-    path('reports/accounts/', views..as_view())
+    path('reports/', views.ReportView.as_view()),
+    # path('reports/plannings/', views..as_view())
+    # path('reports/accounts/', views..as_view())
 
 ]
--- reports/views.py
+++ reports/views.py
@@ -1,10 +1,12 @@
+import ipdb
+from transactions.models import Transaction
+from accounts.models import Account
+from plannings.models import Planning
 from rest_framework.views import APIView, Request, Response, status
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework.permissions import IsAuthenticated
 from django.db.models import Sum
-
-from accounts.models import Account
-from transactions.models import Transaction
+from decimal import Decimal
 
 
 class ReportView(APIView):
@@ -12,22 +14,47 @@ class ReportView(APIView):
     permission_classes = [IsAuthenticated]
 
     def get(self, request: Request) -> Response:
-        user_report = {
-            "inicial_balance": "",
-            "total_income": "",
-            "total_expenses": "",
-            "current_balance": 0
-        }
 
         user_accounts = Account.objects.filter(user_id=request.user.id)
-        user_report['inicial_balance'] = user_accounts.aggregate(
-            Sum('balance'))
-
         user_transactions = Transaction.objects.filter(
             account__in=user_accounts.values_list('id', flat=True))
-        user_report['total_income'] = user_transactions.filter(
-            transaction_type='receipt').aggregate(Sum('amount'))
-        user_report['total_expenses'] = user_transactions.filter(
-            transaction_type='payment').aggregate(Sum('amount'))
+        user_plannings = Planning.objects.filter(
+            account__in=user_accounts.values_list('id', flat=True))
 
-        return Response("", status.HTTP_200_OK)
+        initial_balance = user_accounts.aggregate(Sum('balance'))
+        total_income = user_transactions.filter(
+            transaction_type='receipt').aggregate(Sum('amount'))
+        total_expenses = user_transactions.filter(
+            transaction_type='payment').aggregate(Sum('amount'))
+        current_balance = initial_balance['balance__sum'] + \
+            total_income['amount__sum'] - total_expenses['amount__sum']
+
+        limits_overview = []
+        for account in user_accounts:
+            account_transactions = user_transactions.filter(
+                account_id=account.id)
+            expenses = account_transactions.filter(
+                transaction_type='payment').aggregate(Sum('amount'))
+
+            account_plannings = user_plannings.filter(account_id=account.id)
+            total_planned_expenses = account_plannings.aggregate(
+                Sum('expense'))
+
+            limits_overview.append({
+                'account': account.account_number,
+                'remaining_limit': account.overdraft_limit - (expenses['amount__sum'] if account_transactions.filter(
+                    transaction_type='payment').count() > 0 else Decimal('0.00')),
+                'total_planned_expenses': total_planned_expenses['expense__sum']
+            })
+
+        user_report = {
+            "initial_balance": initial_balance['balance__sum'],
+            "total_income": total_income['amount__sum'],
+            "total_expenses": total_expenses['amount__sum'],
+            "current_balance": current_balance,
+            'limits_overview': limits_overview
+        }
+
+        # ipdb.set_trace()
+
+        return Response(user_report, status.HTTP_200_OK)
