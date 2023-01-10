19ff8207b96be48ba35e1fbc34b84de2d63a63e1
VictrCruz312 <motoggb979@gmail.com>
2023-01-10 15:15:50 -0300

    feat: configurando crontab para deleção de usuário inativo

--- users/urls.py
+++ users/urls.py
@@ -1,13 +1,12 @@
 from django.contrib import admin
 from django.urls import path
-from rest_framework_simplejwt.views import TokenObtainPairView
-from .views import UserView, UserDetailView
+from .views import UserView, UserDetailView, PersonalizedTokenObtainPairView
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path("users/", UserView.as_view()),
     path("users/<str:user_uuid>/", UserDetailView.as_view()),
-    path("login/", TokenObtainPairView.as_view()),
+    path("login/", PersonalizedTokenObtainPairView.as_view()),
     path("schema/", SpectacularAPIView.as_view(), name="schema"),
     path(
         "docs/",
