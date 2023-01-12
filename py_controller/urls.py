5483928a7945d1dd710be725f78e65bc016e1f9a
Victor Cruz <103138539+VictrCruz312@users.noreply.github.com>
2023-01-12 11:06:21 -0300

    fix
    removendo as urls de address

--- py_controller/urls.py
+++ py_controller/urls.py
@@ -21,7 +21,6 @@ urlpatterns = [
     path("api/", include("plannings.urls")),
     path("api/", include("accounts.urls")),
     path("api/", include("users.urls")),
-    path("api/", include("address.urls")),
     path("api/", include("transactions.urls")),
     path("api/", include("reports.urls"))
 ]
